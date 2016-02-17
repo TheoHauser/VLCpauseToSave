d1ba8b582dc1965c60e41cbf562af9498dc5403d
TheoHauser <theo.hauser@temple.edu>
2016-02-17 15:10:37 -0500

    added dialog

--- pauseToSave.lua
+++ pauseToSave.lua
@@ -7,6 +7,7 @@ function descriptor()
 end
 
 function activate()
+  started = true
 end
 
 function deactivate()
@@ -17,4 +18,35 @@ end
 
 function playing_changed()
   vlc.msg.dbg("[Dummy] Status: " .. vlc.playlist.status())
+  if vlc.playlist.status()=="paused" then
+    show_ui()  
+  end
+end
+
+function show_ui()
+  d = vlc.dialog("would you like to save this spot")
+  if started then
+    d:set_title("Start?")
+    d:add_button("Start", log)
+    d:add_button("Do Nothing", hide_ui)
+    d:show()
+  else
+    d:set_title("Stop?")
+    d:add_button("Stop", log)
+    d:add_button("Do Nothing", hide_ui)
+    d:show()
+  end
+end
+
+function log()
+  d:delete();
+  if started then
+    started = false
+  else
+    started = true
+  end
+end
+
+function hide_ui()
+  d:delete()
 end
