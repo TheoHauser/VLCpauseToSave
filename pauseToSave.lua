0518177c4bd51b6d16f471ede6bb8fbea1d05582
TheoHauser <theo.hauser@temple.edu>
2016-02-26 17:42:25 -0500

    done

--- pauseToSave.lua
+++ pauseToSave.lua
@@ -12,10 +12,11 @@ function activate()
   started = false
   input = ""
   stringTime = ""
-  item = vlc.input.item()
-  name = item:name()
-  vlc.msg.dbg("name =".. name)
-  get_time()
+  fileCreated = false
+  local item = vlc.input.item()
+  if not (item==nil) then
+    create_file()
+  end
 end
 
 function deactivate()
@@ -28,22 +29,29 @@ end
 
 function playing_changed()
   vlc.msg.dbg("[Dummy] Status: " .. vlc.playlist.status())
+  if vlc.playlist.status()=="playing" then
+    if not fileCreated then 
+      create_file()
+    end
+  end
   if vlc.playlist.status()=="paused" then
     show_ui()  
   end
   if vlc.playlist.status()=="stopped" then
-    save_file()
+    close_file()
   end
 end
 
 function show_ui()
   d = vlc.dialog("would you like to save this spot")
   if not started then
+    timest = get_time()
     d:set_title("Start?")
     d:add_button("Start", log)
     d:add_button("Do Nothing", hide_ui)
     d:show()
   else
+    timend = get_time()
     d:set_title("Stop?")
     d:add_button("Stop", log)
     d:add_button("Do Nothing", hide_ui)
@@ -76,22 +84,38 @@ function hide_ui()
 end
 
 function write_to_file()
-  file = io.open("/home/jimmy/test.txt","a+")
-  file:write(input .. "\n")
-  file:close()
+  if not fileCreated then 
+    create_file()
+  end
+  file:write(timest .. " to " .. timend .. " :" ..  input .. "\n")
+  vlc.msg.dbg("Output time  = " .. timest .. "to " .. timend)
 end
 
-function save_file()
-  
+function close_file()
+  file:write("\n")
+  file:close()
+  fileCreated = false
+end
+
+function create_file()
+  vlc.msg.dbg("in createFile")
+  fileCreated = true
+  get_time()
+  file = io.open("/home/hauser2016/Documents/VLCoutput/" .. os.date("%m-%d-%Y", time) .. ".txt","a+")
+  file:write("Annotated on: " .. os.date() .. "\n------------------------------------------------\n\n")
 end
 
 function get_time()
   vid = vlc.input.item()
+  obj = vlc.object.input()
   name = vid:name()
 
-  stringTime = name:sub(14, 15)
-  vlc.msg.dbg(stringTime)
+  timeList = {month=name:sub(9,10) , day=name:sub(11,12), year=name:sub(5,8), hour=name:sub(14,15), min=name:sub(16,17), sec = name:sub(18,19)}
+  time = os.time(timeList) 
+  currentTime = time + vlc.var.get(obj, "time")
+  timestr = os.date("%H:%M:%S", currentTime)
+  vlc.msg.dbg("Time = " .. timestr)
 
-  stringTime = stringTime .. ":"
-  vlc.msg.dbg(stringTime)
+  return timestr
+ 
 end
